f7696d281715cd9e580ea937f5f0607d90823603
Ismail <iii409475@gmail.com>
2024-07-10 23:08:50 +0200

    added the loading screen

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -3,12 +3,11 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "./Movie.css";
 import { useParams } from "react-router-dom";
-import Genres from "../data/Genres";
 import { FaDownload } from "react-icons/fa";
 import { BiSolidMoviePlay } from "react-icons/bi";
 import { FaStar } from "react-icons/fa6";
 import { FaImdb } from "react-icons/fa";
-import no_avatar from '../assets/no-avatar.jpg'
+import no_avatar from "../assets/no-avatar.jpg";
 
 const Movie = () => {
   const { id } = useParams();
@@ -32,8 +31,6 @@ const Movie = () => {
       .then((response) => response.json())
       .then((data) => {
         setMovieDetails(data);
-        console.log(data);
-        console.log(movieDetails);
       })
       .catch((err) => console.error(err));
 
@@ -41,8 +38,6 @@ const Movie = () => {
       .then((response) => response.json())
       .then((data) => {
         setMovieImages(data.backdrops);
-        console.log(data);
-        console.log(movieImages);
       })
       .catch((err) => console.error(err));
 
@@ -53,8 +48,6 @@ const Movie = () => {
       .then((response) => response.json())
       .then((data) => {
         setSimilarMovies(data.results);
-        console.log(data);
-        console.log(similarMovies);
       })
       .catch((err) => console.error(err));
 
@@ -65,8 +58,6 @@ const Movie = () => {
       .then((response) => response.json())
       .then((data) => {
         setMovieReviews(data.results);
-        console.log(data);
-        console.log(similarMovies);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -74,7 +65,16 @@ const Movie = () => {
   return (
     <>
       <Header />
+
+     
+
       <div className="movie-page-container">
+
+      {
+
+        movieDetails && movieImages && movieReviews ? 
+        
+        <>
         <div
           className="page-background-image"
           style={{
@@ -230,28 +230,30 @@ const Movie = () => {
                       <div className="review" key={index}>
                         <div className="avatar-name-date">
                           <div className="avatar">
-                            {review.author_details.avatar_path ? <img
-                              src={`https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`}
-                              alt="avatar"
-                            /> : <img
-                            src={no_avatar}
-                            alt="no-avatar"
-                          />}
+                            {review.author_details.avatar_path ? (
+                              <img
+                                src={`https://image.tmdb.org/t/p/original${review.author_details.avatar_path}`}
+                                alt="avatar"
+                              />
+                            ) : (
+                              <img src={no_avatar} alt="no-avatar" />
+                            )}
                           </div>
                           <div className="name-date">
-                            <div className="name">{review.author_details.username}</div>
-                            <div className="date">{review.updated_at.substring(0, 10)}</div>
+                            <div className="name">
+                              {review.author_details.username}
+                            </div>
+                            <div className="date">
+                              {review.updated_at.substring(0, 10)}
+                            </div>
                           </div>
                         </div>
 
-                              <div className="rating">
-                                Rating: {review.author_details.rating} 
-                              </div>
+                        <div className="rating">
+                          Rating: {review.author_details.rating} / 10
+                        </div>
 
-                              <div className="comment">
-                                {review.content}
-                              </div>
-
+                        <div className="comment">{review.content}</div>
                       </div>
                     );
                   })
@@ -264,6 +266,35 @@ const Movie = () => {
             </div>
           </div>
         </div>
+        
+        </>
+        
+        :
+
+
+        <>
+        
+        <div className="loading-screen">
+          <div className="loader"></div>
+          {/* <div className="loader-2"></div> */}
+        </div>
+
+
+        
+        </>
+
+
+
+
+
+
+      }
+        
+    
+
+
+
+        
       </div>
       <Footer />
     </>
