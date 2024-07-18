2b63188ca8d3c9de2fcf781bb523ffbeba4201ba
Ismail <68245583+IsmailDlamini@users.noreply.github.com>
2024-07-18 13:33:47 +0200

    Update Ai.jsx

--- src/utils/Ai.jsx
+++ src/utils/Ai.jsx
@@ -1,8 +1,19 @@
-
+import { useRef } from "react";
+import {
+  HarmCategory,
+  HarmBlockThreshold,
+  GoogleGenerativeAI,
+} from "@google/generative-ai";
+import ReactMarkdown from "react-markdown";
+import { IoClose } from "react-icons/io5";
+import { SlOptionsVertical } from "react-icons/sl";
 
 
 const Ai = () => {
-  return "Hello world"
+
+
+  
+  return "Hello world";
   
 }
 
