f08ccf1c726cc966098507e46b5fdec3480b0078
Ismail <68245583+IsmailDlamini@users.noreply.github.com>
2024-07-18 13:27:29 +0200

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -160,7 +160,7 @@ const Pagination = (prop) => {
               ? "disable"
               : ""
           }`}
-               onClick={() => prop.page ? prop.page !== prop.total_pages ? changePage((+prop.page + 1)) : changePage(prop.total_pages > 1 ? 2): ""}
+               onClick={() => prop.page ? +prop.page !== +prop.total_pages ? changePage((+prop.page + 1)) : changePage(+prop.total_pages > 1 ? 2 : 1): ""}
 {/*                 onClick={() => changePage(prop.page ? prop.page !== prop.total_pages ? (+prop.page + 1) : "" : prop.total_pages > 1 ? 2 : "")} */}
         >
           Next {">>"}
