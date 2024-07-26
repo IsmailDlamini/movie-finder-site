2d099493b6138cddc3bf5f7a7b2cdd05f82703af
Ismail <iii409475@gmail.com>
2024-07-26 16:30:31 +0200

    pushing hint extra code

--- src/components/ChatBotIcon.jsx
+++ src/components/ChatBotIcon.jsx
@@ -5,22 +5,30 @@ import { IoCloseCircle } from "react-icons/io5";
 import { useState } from "react";
 
 const ChatBotIcon = ({ changeChatBotState, chatBotState, showHint }) => {
+  const searchParams = new URLSearchParams(location.search);
+
+  const page = searchParams.get("page");
+
   const [hintMessage, setHintMessage] = useState(true);
 
   return (
     <>
-      {showHint ? hintMessage && (
-        <div className="chat-bot-availability-hint">
-          hello.👋 My name is Ismail the movie bot and I am here to assist you
-          with movie🎬 recommendations ✨
-          <IoCloseCircle
-            id="close-message-icon"
-            onClick={() => {
-              setHintMessage(!hintMessage);
-            }}
-          />
-        </div>
-      ) : ""}
+      {!page
+        ? showHint
+          ? hintMessage && (
+              <div className="chat-bot-availability-hint">
+                hello.👋 My name is Ismail the movie bot and I am here to assist
+                you with movie🎬 recommendations ✨
+                <IoCloseCircle
+                  id="close-message-icon"
+                  onClick={() => {
+                    setHintMessage(!hintMessage);
+                  }}
+                />
+              </div>
+            )
+          : ""
+        : ""}
 
       <div className="chat-bot-icon">
         <img
