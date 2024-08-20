daa09c2e915c473aa1d2023426cb4c19c1c9e7b1
Ismail <iii409475@gmail.com>
2024-08-21 00:06:10 +0200

    pulling major changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,6 @@ import ReactGA from "react-ga4";
 import MovieObject from "../components/MovieObject";
 import SearchFilter from "../components/SearchFilter";
 import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useParams } from "react-router-dom";
 
 
 const Home = () => {
