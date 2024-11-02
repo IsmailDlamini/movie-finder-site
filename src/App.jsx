bd696c8b31ce87c32e1e28e1dae692958f917b62
Ismail <iii409475@gmail.com>
2024-11-02 03:33:44 +0200

    remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 import Search from './pages/Search';
